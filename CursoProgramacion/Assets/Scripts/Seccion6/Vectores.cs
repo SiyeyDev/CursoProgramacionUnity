67d80409a5f7ae2c0f452f96198d91ae7dce6d41
SiyeyDev <siyeydev@gmail.com>
2020-10-05 17:52:21 -0500

    Seccion 6 - Vectores

---
+++ CursoProgramacion/Assets/Scripts/Seccion6/Vectores.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vectores : MonoBehaviour
+{
+    public Vector3 pos;
+    public float factor;
+    public Transform esfera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+        esfera.localScale  = Vector3.one * factor;
+        esfera.position = new Vector3(esfera.position.x, pos.y, esfera.position.z);
+        esfera.transform.forward = transform.forward;
+
+        Debug.Log(Vector3.Distance(transform.position, esfera.position));
+        Debug.Log(pos - esfera.position );
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
