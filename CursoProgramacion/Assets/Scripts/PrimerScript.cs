08405b7a0e7a9e10c021c3e8cb3e5ff03adfbcb2
SiyeyDev <siyeydev@gmail.com>
2020-10-05 13:07:17 -0500

    Ejercicio Clase 3 - Lógica Unity

---
+++ CursoProgramacion/Assets/Scripts/PrimerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimerScript : MonoBehaviour
+{
+    public int x, y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(SumaVariables());
+        ActualizaVariables(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    private int SumaVariables()
+    {
+        return x + y;
+    }
+
+    private void ActualizaVariables(int f)
+    {
+        x *= f;
+        y *= f;
+    }
+
+
+}
