c0c80b7ce364f6776798ef0109cc16a737d647ba
SiyeyDev <siyeydev@gmail.com>
2020-10-05 17:27:09 -0500

    Seccion 5 - Ejercicio Salud PJ

---
+++ CursoProgramacion/Assets/Scripts/Seccion5/Character.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public string characterName;
+    public characterRaza raza;
+
+
+    public enum characterRaza
+    {
+        humano,enano,elfo
+    }
+
+    public string GetDescription()
+    {
+        return (characterName + "the " + raza);
+    }
+}
---
+++ CursoProgramacion/Assets/Scripts/Seccion5/Health.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    private int initialHealth;
+    public bool invunerability,hasShield;
+    public int shield=10;
+    private Character chara;
+
+
+
+    private void Awake()
+    {
+        chara = GetComponent<Character>();
+    }
+
+    void Start()
+    {
+        Debug.Log(chara.GetDescription());
+        Debug.Log(shield);
+
+        switch (chara.raza)
+        {
+            case Character.characterRaza.humano:
+                health = 100;
+                initialHealth = health;
+                SetShield(true);
+                TakeDamage(25);
+                Debug.Log(GetHealthRatio() + " % antes de la cura");
+                RecuperarSalud();
+                Debug.Log(GetHealthRatio() + " % Despues de la cura");
+
+                break;
+
+            case Character.characterRaza.enano:
+                health = 150;
+                initialHealth = health;
+                MultiAttack(4, 10);
+                break;
+
+            case Character.characterRaza.elfo:
+                health = 125;
+                initialHealth = health;
+                CheckInvunerability(true);
+                TakeDamage(25);
+              
+                break;
+        }
+
+
+      
+
+
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (invunerability)
+        {
+            Debug.Log("Es Invunerable");
+
+        }
+        else if (hasShield)
+        {
+            health = shield - damage;
+            Debug.Log("El personaje pierde " + health + " puntos de vida");
+        } else
+        {
+            health -= damage;
+            Debug.Log("Character attacked " + damage);
+        }
+    }
+
+
+    public  void MultiAttack(int ataques,int damage)
+    {
+        for(int i =0; i < ataques; i++)
+        {
+            TakeDamage(damage);
+        }
+
+    }
+
+    public float GetHealthRatio()
+    {
+        return ((float)health / initialHealth)*100;
+    }
+
+    public void CheckInvunerability(bool i)
+    {
+        invunerability = i;
+    }
+
+
+    public void SetShield(bool i)
+    {
+        hasShield = true;
+    }
+
+    public void RecuperarSalud()
+    {
+        for (int i = health; i < initialHealth; i++)
+        {
+            health += 1;
+        }
+            
+            
+    }
+}
